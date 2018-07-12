083dd5cb478c1ff9448ae73f68ce94fa960425f7
Ashyah Hewage <ashyahgalbokke@gmail.com>
2018-07-12 21:52:40 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,24 @@
+class dog
+  def initialize(bark, sit)
+    @bark = bark
+    @sit = sit
+  end
+end
+
+def bark
+  @bark
+end
+
+def sit
+  @sit
+end
+
 # person.rb
 class person
   def initialize(talk, walk)
     @talk = talk
-    @walk = walk
+    @walk = 
+  end
     
 
 end
