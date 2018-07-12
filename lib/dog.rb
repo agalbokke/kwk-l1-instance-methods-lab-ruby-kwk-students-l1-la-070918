bde42d350de33e0515b438fd7fae0180442d0410
Ashyah Hewage <ashyahgalbokke@gmail.com>
2018-07-12 21:56:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,18 @@
-# dog.rb
-# class dog
-#   def initialize(bark, sit)
-#     @bark = bark
-#     @sit = sit
+class dog
+  def initialize(bark, sit)
+    @bark = bark
+    @sit = sit
     
-#   end
-# end
-
-# def bark
-#   @bark
-#   puts "Woof!"
-# end
-
-# def sit
-#   @sit
-#   puts "The Dog is sitting."
-# end
-
-class Dog
-  def bark
-    puts "Woof!"
-  end
-
-  def sit
-    puts "The Dog is sitting"
   end
 end
+
+def bark
+  @bark
+  puts "Woof!"
+end
+
+def sit
+  @sit
+  puts "The Dog is sitting."
+end
+
